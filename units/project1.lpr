c2e6c141c563b9084ec667628364ffa09a2c5b02
Alexandre <alexandre.azevedo.mrus@gmail.com>
2020-09-08 16:18:25 -0300

    Finalizando
    Finalizando

--- units/project1.lpr
+++ units/project1.lpr
@@ -16,14 +16,17 @@ uses
 begin
   RequireDerivedFormResource:=True;
   Application.Initialize;
-  Application.CreateForm(TfrmPrincipal, frmPrincipal );
-  Application.CreateForm(TfrmPadrao, frmPadrao);
   Application.CreateForm(TdtmGlobal, dtmGlobal);
-  Application.CreateForm(TfrmCadProduto, frmCadProduto);
-  Application.CreateForm(TfrmPesquisa, frmPesquisa);
-  Application.CreateForm(TfrmLanPedidoVenda, frmLanPedidoVenda);
-  Application.CreateForm(TfrmFinalizaPedidoVenda, frmFinalizaPedidoVenda);
-  Application.CreateForm(TfrmEncerraPedido, frmEncerraPedido);
-  Application.Run;
+  if dtmGlobal.ConectaServidor then
+  begin
+    Application.CreateForm(TfrmPrincipal, frmPrincipal );
+    Application.CreateForm(TfrmPadrao, frmPadrao);
+    Application.CreateForm(TfrmCadProduto, frmCadProduto);
+    Application.CreateForm(TfrmPesquisa, frmPesquisa);
+    Application.CreateForm(TfrmLanPedidoVenda, frmLanPedidoVenda);
+    Application.CreateForm(TfrmFinalizaPedidoVenda, frmFinalizaPedidoVenda);
+    Application.CreateForm(TfrmEncerraPedido, frmEncerraPedido);
+    Application.Run;
+  end;
 end.
 
--- units/udtmglobal.pas
+++ units/udtmglobal.pas
@@ -5,7 +5,7 @@ unit uDtmGlobal;
 interface
 
 uses
-  Classes, SysUtils, IBConnection, sqldb, DB, FileUtil;
+  Classes, SysUtils, IBConnection, sqldb, DB, FileUtil, Dialogs;
 
 type
 
@@ -68,13 +68,16 @@ type
     transProduto: TSQLTransaction;
     transPedido: TSQLTransaction;
     procedure DataModuleCreate(Sender: TObject);
+    procedure qryItemPedidoAfterDelete(DataSet: TDataSet);
     procedure qryItemPedidoAfterPost(DataSet: TDataSet);
     procedure qryPedidoAfterDelete(DataSet: TDataSet);
     procedure qryPedidoAfterPost(DataSet: TDataSet);
     procedure qryProdutoAfterPost(DataSet: TDataSet);
   private
-
+    procedure LerConfiguracao;
   public
+    sCaminhoBanco: string;
+    function ConectaServidor: boolean;
 
   end;
 
@@ -95,13 +98,19 @@ end;
 
 procedure TdtmGlobal.DataModuleCreate(Sender: TObject);
 begin
-  qryProduto.Transaction:=transProduto;
-  qryPedido.Transaction:=transPedido;
-  qryItemPedido.Transaction:=transItemPedido;
-  qryConsultaPedido.Transaction:=transConsultaPedido;
-  qryBuscaProximoItemPedido.Transaction:=transBuscaProximoItem;
-  qryBuscaProduto.Transaction:=transConsultaProduto;
-  qryParcelaPedido.Transaction:=transParcelaPedido;
+  qryProduto.Transaction := transProduto;
+  qryPedido.Transaction := transPedido;
+  qryItemPedido.Transaction := transItemPedido;
+  qryConsultaPedido.Transaction := transConsultaPedido;
+  qryBuscaProximoItemPedido.Transaction := transBuscaProximoItem;
+  qryBuscaProduto.Transaction := transConsultaProduto;
+  qryParcelaPedido.Transaction := transParcelaPedido;
+end;
+
+procedure TdtmGlobal.qryItemPedidoAfterDelete(DataSet: TDataSet);
+begin
+  qryItemPedido.ApplyUpdates;
+  transItemPedido.CommitRetaining;
 end;
 
 procedure TdtmGlobal.qryPedidoAfterDelete(DataSet: TDataSet);
@@ -122,5 +131,53 @@ begin
   transProduto.CommitRetaining;
 end;
 
-end.
 
+procedure TdtmGlobal.LerConfiguracao;
+var
+  arq: TextFile; { declarando a variável "arq" do tipo arquivo texto }
+  linha: string;
+begin
+
+  // [ 1 ] Associa a variável do programa "arq" ao arquivo externo "tabuada.txt"
+  //       na unidade de disco "d"
+ { AssignFile(arq, Application.ExeName, '.ini');
+
+  {$I-}// desativa a diretiva de Input
+  Reset(arq);   // [ 3 ] Abre o arquivo texto para leitura
+  {$I+}// ativa a diretiva de Input
+
+  if (IOResult <> 0) // verifica o resultado da operação de abertura
+  then
+    ShowMessage('Erro na leitura do ini !!!')
+  else
+  begin
+    // [ 11 ] verifica se o ponteiro de arquivo atingiu a marca de final de arquivo
+    while (not EOF(arq)) do
+    begin
+      readln(arq, linha); // [ 6 ] Lê uma linha do arquivo
+      sCaminhoBanco := (linha);
+    end;
+
+    CloseFile(arq); // [ 8 ] Fecha o arquivo texto aberto
+  end;  }
+end;
+
+
+function TdtmGlobal.ConectaServidor: boolean;
+begin
+  conexao.Connected := False;
+  LerConfiguracao;
+  conexao.DatabaseName :='H:\GitHub\ProjetoDelphi\PROJETO.FDB';
+  try
+    conexao.Connected := True;
+    Result := True;
+  except
+    begin
+      Result := False;
+      ShowMessage('Não foi possível efetuar a conexão do o Banco de Dados em ' +
+        conexao.DatabaseName);
+    end;
+  end;
+end;
+
+end.
